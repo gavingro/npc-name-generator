3445ff2609e508ccd7e25130159eebb71052d161
Gavin Grochowski <grochowskigavin@gmail.com>
2020-08-27 12:07:35 -0700

    added remove_name() function and testing

--- generator_script.py
+++ generator_script.py
@@ -8,19 +8,38 @@ class Name_Generator:
     """
 
     def __init__(self, f_name_file, l_name_file):
+        self.f_name_file = f_name_file
+        self.l_name_file = l_name_file
         self.f_names = get_names(f_name_file)
         self.l_names = get_names(l_name_file)
 
     def generate_name(self):
+        """Returns a tuple of a random name from the first and last name lists."""
         first_name = choice(self.f_names)
         last_name = choice(self.l_names)
-        return first_name + " " + last_name
+        return first_name, last_name
+
+    def remove_name(self, name_tuple):
+        """Takes a name tuple and removes them from the files on this Generator."""
+        f_name, l_name = name_tuple
+        with open(self.f_name_file, "r") as f_name_file:
+            f_name_text = f_name_file.read()
+        with open(self.f_name_file, "w") as f_name_file:
+            f_name_text_replaced = f_name_text.replace(f_name + ",", "")
+            f_name_text_cleaned = f_name_text_replaced.replace("\n\n", "\n")
+            f_name_file.write(f_name_text_cleaned)
+        with open(self.l_name_file, "r") as l_name_file:
+            l_name_text = l_name_file.read()
+        with open(self.l_name_file, "w") as l_name_file:
+            l_name_text_replaced = l_name_text.replace(l_name + ",", "")
+            l_name_text_cleaned = l_name_text_replaced.replace("\n\n", "\n")
+            l_name_file.write(l_name_text_cleaned)
 
 
 def get_names(filename_string):
     """
-    Gets the string of line/comma seperated names at the filename_string and returns
-    them in a clean, stripped list to be worked with.
+    Gets the string of line/comma seperated names at the filename_string
+    and returns them in a clean, stripped list.
     """
     with open(filename_string) as file_contents:
         name_list = []
@@ -30,7 +49,3 @@ def get_names(filename_string):
             name = name.strip(",")
             name_list.append(name)
     return name_list
-
-
-# Placeholder for testing purposes.
-# Eventually to be replaced by name generator class.
--- test.py
+++ test.py
@@ -18,8 +18,26 @@ class TestNameGenerator(unittest.TestCase):
         self.assertEqual(type(base_name_gen.f_names), list)
         self.assertEqual(type(base_name_gen.f_names[0]), str)
 
-    def test_generator_class_generate(self):
-        self.assertEqual(type(base_name_gen.generate_name()), str)
+    def test_generator_class_generate_func(self):
+        self.assertEqual(type(base_name_gen.generate_name()), tuple)
+
+    def test_generator_class_remove_name_func(self):
+
+        with open("f_names.txt", "a") as f_names:
+            f_names.write("Test,\n")
+        with open("l_names.txt", "a") as l_names:
+            l_names.write("Testson,\n")
+
+        test_name = ("Test", "Testson")
+        base_name_gen.remove_name(test_name)
+        with open("f_names.txt", "r") as f_names:
+            f_names_text = f_names.read()
+            self.assertNotIn("Test", f_names_text)
+            self.assertNotIn("\n\n", f_names_text)
+        with open("l_names.txt", "r") as l_names:
+            l_names_text = l_names.read()
+            self.assertNotIn("Testson", l_names_text)
+            self.assertNotIn("\n\n", l_names_text)
 
 
 if __name__ == "__main__":
